0d7e13d8a11d27f4d40301b034d50aad7155a8ee
acmerfight <acmerfight@gmail.com>
2017-02-28 23:08:44 +0800

    init tinyjson.h

--- tinyjson.h
+++ tinyjson.h
@@ -1,6 +1,28 @@
 #ifndef TINYJSON_LIBRARY_H
 #define TINYJSON_LIBRARY_H
 
-void hello();
+typedef enum {
+    TINY_NULL,
+    TINY_FALSE,
+    TINY_TRUE,
+    TINY_NUMBER,
+    TINY_STRING,
+    TINY_ARRAY,
+    TINY_OBJECT
+} tiny_type;
+
+typedef struct {
+    tiny_type type;
+} tiny_value;
+
+enum {
+    TINY_PARSE_OK = 0,
+    TINY_PARSE_EXCEPT_VALUE,
+    TINY_PARSE_INVALID_VALUE,
+    TINY_PARSE_ROOT_NOT_SINGULAR
+};
+
+int tiny_parse(tiny_value* v, const char* json);
+tiny_type tiny_get_type(const tiny_value* v);
 
 #endif
