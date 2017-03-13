70011689566a09ae386f2fb69157ab885ce1b5d9
acmerfight <acmerfight@gmail.com>
2017-03-13 23:03:32 +0800

    add some declaration

--- tinyjson.h
+++ tinyjson.h
@@ -12,7 +12,13 @@ typedef enum {
 } tiny_type;
 
 typedef struct {
-    double n;
+    union {
+        struct {
+            char* s;
+            size_t len;
+        } s;
+        double n;
+    };
     tiny_type type;
 } tiny_value;
 
@@ -21,11 +27,30 @@ enum {
     TINY_PARSE_EXCEPT_VALUE,
     TINY_PARSE_INVALID_VALUE,
     TINY_PARSE_ROOT_NOT_SINGULAR,
-    TINY_PARSE_NUMBER_TOO_BIG
+    TINY_PARSE_NUMBER_TOO_BIG,
+    TINY_PARSE_MISS_QUOTATION_MARK,
+    TINY_PARSE_INVALID_STRING_ESCAPE,
+    TINY_PARSE_INVALID_STRING_CHAR
 };
 
+#define tiny_init(v) do {(v) -> type = TINY_NULL;} while(0)
+
 int tiny_parse(tiny_value* v, const char* json);
+
+void tiny_free(tiny_value* v);
+
 tiny_type tiny_get_type(const tiny_value* v);
+
+#define tiny_set_null(v) tiny_free(v)
+
+int tiny_get_boolean(const tiny_value* v);
+void tiny_set_boolean(tiny_value* v, int b);
+
 double tiny_get_number(const tiny_value* v);
+void tiny_set_number(tiny_value* v, double n);
+
+const char* tiny_get_string(const tiny_value* v);
+size_t tiny_get_string_length(const tiny_value* v);
+void tiny_set_string(tiny_value* v, const char* s, size_t len);
 
 #endif
